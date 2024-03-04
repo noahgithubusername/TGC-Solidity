bec1abf404ef43c74516115ecf3fe33822ff6c18
noahgithubusername <anombres@gmail.com>
2024-03-04 19:58:28 +0800

    Albert edited storage

--- lesson1/activity2/storage.sol
+++ lesson1/activity2/storage.sol
@@ -8,23 +8,9 @@ pragma solidity >=0.8.2 <0.9.0;
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 contract Storage {
-    uint256 public unlockTime;
-    uint256 public number;
-    uint8[10] public dataArray;
-
-    constructor(uint256 duration) {
-        uint256 currentTime = block.timestamp; // The current time when the smart contract is deployed
-        unlockTime = currentTime + duration; // Assigning unlockTime using duration parameter
-
-        uint8 i = 1;
-        while(i < 11) {
-            dataArray[i-1] = i;
-            i++;
-        }
-    }
-
-    // You can add other functions or modify the contract as needed.
-}
+    uint256 unlockTime;
+    uint256 number;
+    uint8[10] dataArray;
 
     // TODO: Task 1
     // 1. Change the constructor function to take in a parameter of type uint256 called duration
@@ -33,7 +19,22 @@ contract Storage {
     // CODE BLOCK //
     constructor() {
     // END OF CODE BLOCK //
+
+        uint256 currentTime = block.timestamp; // The current time when the smart contract is deployed
+
+        // CODE BLOCK //
+
+
+        // END OF CODE BLOCK //
+
+        uint8 i = 1;
+        while(i < 11) {
+            dataArray[i-1] = i;
+            i++;
+        }
     }
+
+
     /**
      * @dev Store value in variable
      * @param num value to store
@@ -46,16 +47,12 @@ contract Storage {
      * @dev Return value
      * @return value of 'number'
      */
-    function retrieve() public view returns (uint256) {
-    uint256 timeNow = block.timestamp; // The current time when the retrieve function is called
+    function retrieve() public view returns (uint256){
+        uint256 timeNow = block.timestamp; // The current time when the retrieve function is called
 
-    // Task 2: Check if timeNow is greater than unlockTime
-    if (timeNow > unlockTime) {
-        return number; // Return number if timeNow is greater than unlockTime
-    } else {
-        return 0; // Return 0 if timeNow is not greater than unlockTime
-    }
-}
+        // CODE BLOCK //
+
+        return number;
 
         // END OF CODE BLOCK //
 
@@ -70,16 +67,13 @@ contract Storage {
      * @param n how many numbers you would like to add up
      * @return sum of the n number of items in the data array
      */
-    function getSum(uint256 n) public view returns (uint8) {
-    require(n <= 10, "Input should be less than or equal to 10");
+    function getSum(uint256 n) public view returns (uint8){
+        require(n < 10, "Input should be less than or equal to 10");
+        // CODE BLOCK //
+        uint8 sum = 0;
 
-    // Task 3: Use a control flow to add up the contents of the dataArray
-    uint8 sum = 0;
-    for (uint256 i = 0; i < n; i++) {
-        sum += dataArray[i]; // Add the value at index i to sum
-    }
-    return sum;
-}
+
+        return sum;
         // END OF CODE BLOCK //
 
         // TODO: Task 3
