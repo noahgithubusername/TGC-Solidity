855d751a3848573b229e5d1cfe0300d897a5c0a9
noahgithubusername <anombres@gmail.com>
2024-03-04 13:33:04 +0800

    Albert edited storage.sol

--- lesson1/activity2/storage.sol
+++ lesson1/activity2/storage.sol
@@ -8,24 +8,13 @@ pragma solidity >=0.8.2 <0.9.0;
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 contract Storage {
-    uint256 unlockTime;
-    uint256 number;
-    uint8[10] dataArray;
+    uint256 public unlockTime;
+    uint256 public number;
+    uint8[10] public dataArray;
 
-    // TODO: Task 1
-    // 1. Change the constructor function to take in a parameter of type uint256 called duration
-    // 2. Assign the state variable unlockTime to be the addition of currentTime and duration
-
-    // CODE BLOCK //
-    constructor() {
-    // END OF CODE BLOCK //
-
+    constructor(uint256 duration) {
         uint256 currentTime = block.timestamp; // The current time when the smart contract is deployed
-
-        // CODE BLOCK //
-
-
-        // END OF CODE BLOCK //
+        unlockTime = currentTime + duration; // Assigning unlockTime using duration parameter
 
         uint8 i = 1;
         while(i < 11) {
@@ -34,7 +23,17 @@ contract Storage {
         }
     }
 
+    // You can add other functions or modify the contract as needed.
+}
 
+    // TODO: Task 1
+    // 1. Change the constructor function to take in a parameter of type uint256 called duration
+    // 2. Assign the state variable unlockTime to be the addition of currentTime and duration
+
+    // CODE BLOCK //
+    constructor() {
+    // END OF CODE BLOCK //
+    }
     /**
      * @dev Store value in variable
      * @param num value to store
@@ -47,12 +46,16 @@ contract Storage {
      * @dev Return value
      * @return value of 'number'
      */
-    function retrieve() public view returns (uint256){
-        uint256 timeNow = block.timestamp; // The current time when the retrieve function is called
+    function retrieve() public view returns (uint256) {
+    uint256 timeNow = block.timestamp; // The current time when the retrieve function is called
 
-        // CODE BLOCK //
-
-        return number;
+    // Task 2: Check if timeNow is greater than unlockTime
+    if (timeNow > unlockTime) {
+        return number; // Return number if timeNow is greater than unlockTime
+    } else {
+        return 0; // Return 0 if timeNow is not greater than unlockTime
+    }
+}
 
         // END OF CODE BLOCK //
 
@@ -67,13 +70,16 @@ contract Storage {
      * @param n how many numbers you would like to add up
      * @return sum of the n number of items in the data array
      */
-    function getSum(uint256 n) public view returns (uint8){
-        require(n < 10, "Input should be less than or equal to 10");
-        // CODE BLOCK //
-        uint8 sum = 0;
+    function getSum(uint256 n) public view returns (uint8) {
+    require(n <= 10, "Input should be less than or equal to 10");
 
-
-        return sum;
+    // Task 3: Use a control flow to add up the contents of the dataArray
+    uint8 sum = 0;
+    for (uint256 i = 0; i < n; i++) {
+        sum += dataArray[i]; // Add the value at index i to sum
+    }
+    return sum;
+}
         // END OF CODE BLOCK //
 
         // TODO: Task 3
